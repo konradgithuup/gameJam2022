fee7beb4a3e15187f591e4a3ec2459dc900e22a0
MrUnkn0wn <mrunkioiw@gmail.com>
2022-10-29 23:25:39 +0200

    First terrible player moving implementation

--- core/src/com/spielemarmelade/BaseGame.java
+++ core/src/com/spielemarmelade/BaseGame.java
@@ -1,31 +1,73 @@
 package com.spielemarmelade;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 public class BaseGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	Texture img;
+
+	Texture playerTexture;
+	Sprite playerSprite;
+
+	Boolean playerRight = true;
+	int movespeed = 700;
+	OrthographicCamera camera;
+	Rectangle player;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+
+		playerTexture = new Texture(Gdx.files.internal("player.png"));
+		playerSprite = new Sprite(playerTexture);
+
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 1920, 1080);
+		batch = new SpriteBatch();
+
+		player = new Rectangle();
+		player.x = 500;
+		player.y = 500;
+		player.width = 200;
+		player.height = 200;
 	}
 
 	@Override
 	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
+		ScreenUtils.clear(0,0.02f,0.07f, 1);
+		camera.update();
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(playerSprite, player.x, player.y, player.width, player.height);
 		batch.end();
+
+		if(Gdx.input.isKeyPressed(Input.Keys.D)){
+			player.x += movespeed * Gdx.graphics.getDeltaTime();
+			if(!playerRight){
+				playerSprite.flip(true, false);
+				playerRight = true;
+			}
+		}
+		if(Gdx.input.isKeyPressed(Input.Keys.A)){
+			player.x -= movespeed * Gdx.graphics.getDeltaTime();
+			if(playerRight) {
+				playerSprite.flip(true, false);
+				playerRight = false;
+			}
+		}
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+		playerTexture.dispose();
 	}
 }
