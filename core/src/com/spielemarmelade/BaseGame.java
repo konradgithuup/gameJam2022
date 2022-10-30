2e64758abeae62f611dff780ca8a14ee1568eeee
Konrad Ueltzen <konrad.ueltzen@st.ovgu.de>
2022-10-31 00:51:11 +0100

    Add parallax

--- core/src/com/spielemarmelade/BaseGame.java
+++ core/src/com/spielemarmelade/BaseGame.java
@@ -2,15 +2,11 @@ package com.spielemarmelade;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.utils.ScreenUtils;
 
 import java.util.ArrayList;
@@ -21,6 +17,8 @@ public class BaseGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Player player;
 	OrthographicCamera camera;
+	float cameraDeltaX = 0;
+	float cameraDeltaY = 0;
 	ShapeRenderer shapeRenderer;
 
 	int sceneTransition = 0;
@@ -39,7 +37,7 @@ public class BaseGame extends ApplicationAdapter {
 	public void create () {
 		shapeRenderer = new ShapeRenderer();
 		player = new Player();
-		this.level = new Level("level/foreground_front.png");
+		this.level = new Level("level/foreground_front.png", 0);
 		level.addLevelboxes(new LevelBox[]{new LevelBox(1300, 180, 0, 0),
 				new LevelBox(6000, 84, -1000, 0),
 				new LevelBox(300, 3000, -300, 0),
@@ -60,9 +58,9 @@ public class BaseGame extends ApplicationAdapter {
 				new LevelBox(180, 73, 3228, 1488-73),
 				new LevelBox(57, 95, 3680, 1100-95),
 		});
-		Level levelSky = new Level("level/sky.png");
-		Level levelHorizon = new Level("level/horizon.png");
-		Level levelBackForegroundLayer = new Level("level/foreground_back.png");
+		Level levelSky = new Level("level/sky.png", 0.8f);
+		Level levelHorizon = new Level("level/horizon.png", 0.25f);
+		Level levelBackForegroundLayer = new Level("level/foreground_back.png", 0);
 		this.backgroundLevel = new ArrayList<>();
 		this.backgroundLevel.addAll(Arrays.asList(levelSky, levelHorizon, levelBackForegroundLayer));
 
@@ -83,6 +81,7 @@ public class BaseGame extends ApplicationAdapter {
 
 		player.updateInput(level);
 		updateCamera();
+		updateEnvironmentAssets();
 
 		batch.setProjectionMatrix(camera.combined);
 		shapeRenderer.setProjectionMatrix(camera.combined);
@@ -92,7 +91,7 @@ public class BaseGame extends ApplicationAdapter {
 
 		// render level background
 		for (Level l : this.backgroundLevel) {
-			batch.draw(l.sprite, 0, 0, level.sprite.getWidth(), level.sprite.getHeight());
+			batch.draw(l.sprite, l.sprite.getX(), l.sprite.getY(), l.sprite.getWidth(), l.sprite.getHeight());
 		}
 
 		// render character
@@ -119,13 +118,20 @@ public class BaseGame extends ApplicationAdapter {
 		int camLeftBorder = (int) (level.sprite.getX() - player.hitbox.x);
 		int camRightBorder = (int) (level.sprite.getX()+level.sprite.getWidth() - player.hitbox.x);
 
+		this.cameraDeltaX = 0;
+		this.cameraDeltaY = 0;
+
 		if(camLeftBorder < -1534)
-			if(camRightBorder > 1537)
+			if(camRightBorder > 1537) {
+				this.cameraDeltaX = player.hitbox.x - camera.position.x;
 				camera.position.x = player.hitbox.x;
+			}
 
 
 		if (sceneTransition > 0) {
 			if (camera.position.y < CAMERA_TOP_BOUND) {
+				this.cameraDeltaY = CAMERA_SLIDE_SPEED;
+
 				camera.position.y = (camera.position.y + CAMERA_SLIDE_SPEED < CAMERA_TOP_BOUND)?
 						camera.position.y + CAMERA_SLIDE_SPEED : CAMERA_TOP_BOUND;
 			} else {
@@ -133,6 +139,8 @@ public class BaseGame extends ApplicationAdapter {
 			}
 		} else if (sceneTransition < 0) {
 			if (camera.position.y > CAMERA_BOTTOM_BOUND) {
+				this.cameraDeltaY = -CAMERA_SLIDE_SPEED;
+
 				camera.position.y = (camera.position.y - CAMERA_SLIDE_SPEED > CAMERA_BOTTOM_BOUND)?
 						camera.position.y - CAMERA_SLIDE_SPEED : CAMERA_BOTTOM_BOUND;
 			} else {
@@ -146,6 +154,16 @@ public class BaseGame extends ApplicationAdapter {
 		}
 
 		camera.update();
+	}
+
+
+	private void updateEnvironmentAssets() {
+
+		for (Level bg : this.backgroundLevel) {
+			bg.sprite.translate(
+					(float) (cameraDeltaX * bg.getParallaxFactor()),
+					(float) (cameraDeltaY * bg.getParallaxFactor()));
+		}
 	}
 
 	@Override
--- core/src/com/spielemarmelade/Level.java
+++ core/src/com/spielemarmelade/Level.java
@@ -11,15 +11,23 @@ public class Level {
     ArrayList<LevelBox> boxes = new ArrayList<>();
     final Texture texture;
     final Sprite sprite;
+    final float parallaxFactor;
 
-    public Level(String relativeAssetPath){
+    public Level(String relativeAssetPath, float parallaxFactor){
 
         this.texture = new Texture(Gdx.files.internal(relativeAssetPath));
         this.sprite = new Sprite(texture);
+        this.parallaxFactor = parallaxFactor;
     }
 
     void addLevelboxes(LevelBox[] boxes) {
 
         this.boxes.addAll(Arrays.asList(boxes));
+    }
+
+
+    double getParallaxFactor() {
+
+        return this.parallaxFactor;
     }
 }
